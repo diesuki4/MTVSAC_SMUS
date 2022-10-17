a59cb358694344ac3a2ad2812ba165a2bda18523
Kyungbin Cho <diesuki4@gmail.com>
2022-10-17 14:36:21 +0900

    테스트 Push

--- Assets/2. Scripts/CKB/UI/PanelItem.cs
+++ Assets/2. Scripts/CKB/UI/PanelItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PanelItem : MonoBehaviour
 {
@@ -9,32 +10,40 @@ public class PanelItem : MonoBehaviour
         Model       = 2 << 0,
         Thumbnail   = 2 << 1
     }
-    Mode mode;
-
-    Vector3 INVALID_VECTOR3 = new Vector3(int.MinValue, int.MinValue, int.MinValue);
 
     public GameObject goModel;
     public GameObject goThumbnail;
+    public Transform trThumbnailArea;
 
     GameObject model;
+    GameObject thumbnail;
 
-
-    void Start()
-    {
-        mode = Mode.Thumbnail;
-    }
+    void Start() { }
 
     void Update() { }
 
-    Vector3 Raycast(Vector3 mousePosition)
+    bool ScreenPointRaycast(Vector3 screenPoint, out RaycastHit hit, int layerMask = ~0)
     {
-        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
-        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(screenPoint);
 
-        if (Physics.Raycast(ray, out hit, float.MaxValue, 1 << LayerMask.NameToLayer("Floor")))
-            return hit.point;
-        else
-            return INVALID_VECTOR3;
+        return Physics.Raycast(ray, out hit, float.MaxValue, layerMask);
+    }
+
+    void RevalidateItem(Mode mode, Vector3 position)
+    {
+        switch (mode)
+        {
+            case Mode.Model :
+                model.SetActive(true);
+                model.transform.position = position;
+                thumbnail.SetActive(false);
+                break;
+            case Mode.Thumbnail :
+                model.SetActive(false);
+                thumbnail.GetComponent<RectTransform>().anchoredPosition = position;
+                thumbnail.SetActive(true);
+                break;
+        }
     }
 
     public void OnElementPointerDown()
@@ -42,19 +51,30 @@ public class PanelItem : MonoBehaviour
         if (model == null)
             model = Instantiate(goModel);
 
-        model.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+        if (thumbnail == null)
+            thumbnail = Instantiate(goThumbnail, trThumbnailArea);
+
+        RevalidateItem(Mode.Thumbnail, Input.mousePosition);
     }
 
     public void OnElementDrag()
     {
-        
+        RaycastHit hit;
+        bool result = ScreenPointRaycast(Input.mousePosition, out hit);
 
-        if (model)
-            model.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition + Vector3.forward * 10);
+        if (result && model != hit.transform.gameObject && EventSystem.current.IsPointerOverGameObject())
+            RevalidateItem(Mode.Model, hit.point);
+        else
+            RevalidateItem(Mode.Thumbnail, Input.mousePosition);
     }
 
     public void OnElementPointerUp()
     {
-        model = null;
+        if (model.activeSelf)
+            model = null;
+        else
+            Destroy(model);
+        
+        Destroy(thumbnail);
     }
 }
