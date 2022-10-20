3932d651a51b7e273b1778db84797cc43e7e186c
Kyungbin Cho <diesuki4@gmail.com>
2022-10-20 17:52:51 +0900

    목요일 작업

---
+++ Assets/2. Scripts/CKB/Timeline/TimelineTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Timeline.Timeline;
+
+public class TimelineTest : MonoBehaviour
+{
+    void Start()
+    {
+        TL_Timeline timeline = new TL_Timeline();
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
