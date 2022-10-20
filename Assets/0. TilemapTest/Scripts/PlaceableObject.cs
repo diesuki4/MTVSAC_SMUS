cd4524c0beb40d68a885b5f228547e3071625dc6
Kyungbin Cho <diesuki4@gmail.com>
2022-10-20 12:43:21 +0900

    타일맵 그리드 테스트 씬 작성

---
+++ Assets/0. TilemapTest/Scripts/PlaceableObject.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceableObject : MonoBehaviour
+{
+    public bool isPlaced;
+    public Vector3Int size;
+
+    Vector3[] vertices;
+
+    void Start()
+    {
+        GetColliderVertexLocalPositions();
+        CalculateSizeInCells();
+    }
+
+    public virtual void Place()
+    {
+        Destroy(GetComponent<ObjectDrag>());
+
+        isPlaced = true;
+    }
+
+    public void Rotate()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+        size = new Vector3Int(size.y, size.x, 1);
+
+        Vector3[] t_vertices = new Vector3[vertices.Length];
+
+        for (int i = 0; i < t_vertices.Length; ++i)
+            t_vertices[i] = vertices[(i + 1) % vertices.Length];
+
+        vertices = t_vertices;
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(vertices[0]);
+    }
+
+    void GetColliderVertexLocalPositions()
+    {
+        BoxCollider bCol = GetComponent<BoxCollider>();
+
+        vertices = new Vector3[4];
+
+        vertices[0] = bCol.center + new Vector3(-bCol.size.x, -bCol.size.y, -bCol.size.z) * 0.5f;
+        vertices[1] = bCol.center + new Vector3(bCol.size.x, -bCol.size.y, -bCol.size.z) * 0.5f;
+        vertices[2] = bCol.center + new Vector3(bCol.size.x, -bCol.size.y, bCol.size.z) * 0.5f;
+        vertices[3] = bCol.center + new Vector3(-bCol.size.x, -bCol.size.y, bCol.size.z) * 0.5f;
+    }
+
+    void CalculateSizeInCells()
+    {
+        Vector3Int[] t_vertices = new Vector3Int[vertices.Length];
+
+        for (int i = 0; i < t_vertices.Length; ++i)
+        {
+            Vector3 worldPos = transform.TransformPoint(vertices[i]);
+            t_vertices[i] = BuildingSystem.Instance.gridLayout.WorldToCell(worldPos);
+        }
+
+        size = new Vector3Int(Mathf.Abs((t_vertices[0] - t_vertices[1]).x), Mathf.Abs((t_vertices[0] - t_vertices[3]).y), 1);
+    }
+}
