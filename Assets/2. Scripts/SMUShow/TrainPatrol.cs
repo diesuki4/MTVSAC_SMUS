d145efc984cb50cccd5a05c0fb9158a725d6b00a
Kyungbin Cho <diesuki4@gmail.com>
2022-10-26 14:22:02 +0900

    프로토타입용 공연 맵 제작

---
+++ Assets/2. Scripts/SMUShow/TrainPatrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainPatrol : MonoBehaviour
+{
+	public enum Mode
+	{
+		PingPong	= 1 << 0,
+		Repeat		= 1 << 1
+	}
+
+    public Transform startPosition;
+    public Transform endPosition;
+	public float speed;
+	public Mode mode;
+
+    void Start() { }
+
+    void Update()
+    {
+		Vector3 dir = endPosition.position - startPosition.position;
+
+		transform.position = startPosition.position + dir.normalized * Point(Time.time * speed, dir.magnitude);
+    }
+
+	float Point(float t, float length)
+	{
+		float value = 0;
+
+		switch(mode)
+		{
+			case Mode.PingPong :	value = Mathf.PingPong(t, length);
+				break;
+			case Mode.Repeat :		value = Mathf.Repeat(t, length);
+				break;
+		}
+
+		return value;
+	}
+}
