043e0a37b6a451317872b46de520a259858c8df4
wlqdprkffo <108355182+wlqdprkffo@users.noreply.github.com>
2022-11-09 15:43:35 +0900

    ui 수정

--- Assets/2. Scripts/CES/CESPlayerMove.cs
+++ Assets/2. Scripts/CES/CESPlayerMove.cs
@@ -1,27 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
 [RequireComponent(typeof(CharacterController))]
-public class CESPlayerMove : MonoBehaviour
+
+public class CESPlayerMove : MonoBehaviourPun
 {
     CharacterController cc;
-    float speed = 10;
+
+    public Animator anim;
+    public GameObject player;
+    PlayerState playerState;
+
+    public float moveSpeed = 10.0f;
+    Vector3 dir;
+
+    // 점프 관련 
+    public float gravity = -50.0f;
+    public float jumpPower = 30.0f;
+    public Transform rayPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        cc = GetComponent<CharacterController>();
+        anim = player.GetComponent<Animator>();
+        cc = transform.GetComponent<CharacterController>();
+        playerState = transform.GetComponent<PlayerState>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
+        SetGravity();
+        PlayerMove();
+    }
 
-        Vector3 dir = new Vector3(h, 0, v).normalized;
+    void SetGravity()
+    {
+        if (PutMousePointer.instance.isWeightlessness == true)
+        {
+            gravity = -10f;
+        }
+        else
+        {
+            gravity = -30f;
+        }
+    }
 
-        cc.Move(speed * dir * Time.deltaTime);
+    void PlayerMove()
+    {
+        if (!photonView.IsMine) return;
+
+        transform.forward = Camera.main.transform.forward;
+
+        //if (photonView.RPC("OnTheGround()",RpcTarget.All)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            dir = new Vector3(h, 0, v);
+            dir.Normalize();
+            //anim.SetFloat("Speed", dir.magnitude);
+
+            dir *= moveSpeed;
+
+            // 점프
+            if (Input.GetButtonDown("Jump"))
+            {
+                dir.y = jumpPower;
+            }
+        }
+        dir.y += gravity * Time.deltaTime;
+
+        cc.Move(dir * Time.deltaTime);
+    }
+
+    // 땅 확인
+    [PunRPC]
+    bool OnTheGround()
+    {
+        Ray ray = new Ray(rayPos.position, Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, 0.1f))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
--- Assets/2. Scripts/CES/ForUI/PutMousePointer.cs
+++ Assets/2. Scripts/CES/ForUI/PutMousePointer.cs
@@ -1,29 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PutMousePointer : MonoBehaviour
 {
-    public Transform weightlessness;
+    public static PutMousePointer instance;
+
+    public Button weightlessness;
+    public Transform weightlessnessImage;
+
+    public bool isWeightlessness = false;
+
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        weightlessness.gameObject.SetActive(false);
+        weightlessnessImage.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isWeightlessness == true)
+        {
+            weightlessness.GetComponent<Image>().color = new Color(102f / 255f, 102f / 255f, 255f / 255f);
+        }
+        else
+        {
+            weightlessness.GetComponent<Image>().color = Color.white;
+        }
     }
 
     public void OnMouseEnter()
     {
-        weightlessness.gameObject.SetActive(true);
+        weightlessnessImage.gameObject.SetActive(true);
     }
     public  void OnMouseExit()
     {
-        weightlessness.gameObject.SetActive(false);
+        weightlessnessImage.gameObject.SetActive(false);
+    }
+
+    public void ChangeGravity()
+    {
+        isWeightlessness = !isWeightlessness;
     }
 }
