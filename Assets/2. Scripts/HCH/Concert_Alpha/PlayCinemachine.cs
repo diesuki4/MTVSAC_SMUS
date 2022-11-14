d85d6ce62609ed53f47e140a703b7fbf84dfa052
leepers97 <111097465+leepers97@users.noreply.github.com>
2022-11-15 04:15:12 +0900

    22

--- Assets/2. Scripts/HCH/Concert_Alpha/PlayCinemachine.cs
+++ Assets/2. Scripts/HCH/Concert_Alpha/PlayCinemachine.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 // P버튼을 누르면 시네머신을 시작하고
 // 캐릭터들의 애니메이션을 실행하고 싶다
 
@@ -17,6 +18,7 @@ public class PlayCinemachine : MonoBehaviour
     SkinnedMeshRenderer smr;
     public bool startCameraMove;
 
+    public Camera firstCam;
     private void Awake()
     {
         instance = this;
@@ -36,12 +38,15 @@ public class PlayCinemachine : MonoBehaviour
     void Update()
     {
         PlayCin();
+        ReloadCin();
+        BackScene();
     }
 
     public void PlayCin()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
+            firstCam.enabled = false;
             // 시네머신을 실행하고
             pd.Play();
             // 작은 캐릭터 애니메이션을 실행하고
@@ -51,6 +56,24 @@ public class PlayCinemachine : MonoBehaviour
             // 큰 캐릭터 meshrenderer를 켜고 싶다 
             smr.enabled = true;
             startCameraMove = true;
+        }
+    }
+
+    public void ReloadCin()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // 씬을 다시 로드하고 싶다
+            SceneManager.LoadScene("Concert_Alpha_HCH");
+        }
+    }
+
+    public void BackScene()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            // 내 공연 씬으로 돌아가고 싶다
+            SceneManager.LoadScene("ArtistConcertListScene_C");
         }
     }
 }
