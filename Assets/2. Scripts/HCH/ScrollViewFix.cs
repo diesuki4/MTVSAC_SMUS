a8929efabfdc986d29a680e7e581b4bb3db395e9
leepers97 <111097465+leepers97@users.noreply.github.com>
2022-11-25 09:50:55 +0900

    스튜디오 UI

---
+++ Assets/2. Scripts/HCH/ScrollViewFix.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Content의 PosY를 -4.3에 고정하고 싶다
+
+public class ScrollViewFix : MonoBehaviour
+{
+    RectTransform rect;
+    public float fixNum = -4.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = this.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, fixNum);
+    }
+}
