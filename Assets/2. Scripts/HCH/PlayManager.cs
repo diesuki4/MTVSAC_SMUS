62a07bb8a3c716b9bfbf4166c097649db9cb20f0
leepers97 <111097465+leepers97@users.noreply.github.com>
2022-11-29 05:24:48 +0900

    show ui

---
+++ Assets/2. Scripts/HCH/PlayManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayManager : MonoBehaviour
+{
+    public GameObject timelineLoader;
+    TimelinePlayer tp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tp = timelineLoader.GetComponent<TimelinePlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            tp.OnClickPlayKeyData();
+        }
+
+    }
+}
---
+++ Assets/2. Scripts/HCH/ShowManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+public class ShowManager : MonoBehaviour
+{
+    public Text push;
+    public RectTransform warningImage;
+    public GameObject warning;
+    Transform namee;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartTheShow();
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            OnClickWarn();
+        }
+    }
+
+    // 공연자 독촉하기
+    void StartTheShow()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) push.gameObject.SetActive(false);
+
+        if (push.gameObject.activeSelf)
+        {
+            if (push.color.a == 1) push.DOFade(0.1f, 1f);
+            else if (push.color.a < 0.11) push.DOFade(1, 1f);
+        }
+    }
+
+    // 경고이미지 띄우기
+    public void OnClickWarn()
+    {
+        warning.SetActive(true);
+    }
+
+    // 경고이미지 없애기
+    public void ClickNoExit()
+    {
+        warning.SetActive(false);
+    }
+
+    public void OnClickBackToMyConcert()
+    {
+        SceneManager.LoadScene("ArtistConcertListScene_C");
+    }
+}
